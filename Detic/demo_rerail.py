7b4a220f435cc51b205cd18eeabe63e8a300a8dd
TofunmiSodimu <35805326+TofunmiSodimu@users.noreply.github.com>
2023-10-19 20:59:09 -0400

    Add files via upload

--- Detic/demo_rerail.py
+++ Detic/demo_rerail.py
@@ -144,17 +144,11 @@ def detect(input,output):
     webcam = None
     ####### DONE #######
 
-    # args = get_parser().parse_args()
-    # setup_logger(name="fvcore")
-    # logger = setup_logger()
-    # logger.info("Arguments: " + str(args))
-
     args = get_parser().parse_args(["--config-file",config_file,"--webcam",webcam,"--input",input,"--output",output,"--vocabulary",vocabulary,
                                     "--opts",opts])
     setup_logger(name="fvcore")
     logger = setup_logger()
     logger.info("Arguments: " + str(args))
-    # print(str(args))
 
     cfg = setup_cfg(args)
 
@@ -167,7 +161,8 @@ def detect(input,output):
         for path in tqdm.tqdm(args.input, disable=not args.output):
             img = read_image(path, format="BGR")
             start_time = time.time()
-            predictions, visualized_output, labels, masks_dets = demo.run_on_image(img)
+            predictions, visualized_output, labels = demo.run_on_image(img)
+            masks_dets = ((predictions["instances"].pred_masks)).tolist()
             confidences = (predictions["instances"].scores).tolist()
             class_names = labels
             boxes = ((predictions["instances"].pred_boxes).tensor).tolist()
@@ -195,7 +190,7 @@ def detect(input,output):
                 cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
                 if cv2.waitKey(0) == 27:
                     break  # esc to quit
-            return boxes,confidences,class_names, masks_dets
+            return boxes,confidences,masks_dets,class_names
     elif args.webcam:
         assert args.input is None, "Cannot have both --input and --webcam!"
         assert args.output is None, "output not yet supported with --webcam!"
@@ -252,132 +247,3 @@ def detect(input,output):
             output_file.release()
         else:
             cv2.destroyAllWindows()
-
-
-    # if __name__ == "__main__":
-    #     mp.set_start_method("spawn", force=True)
-
-    #     ####### SET PARAMETERS #######
-    #     confidence_threshold = 0.5
-    #     config_file = 'configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml'
-    #     # input = 'test.png'
-    #     opts_key = 'MODEL.WEIGHTS'
-    #     opts_value = 'models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth'
-    #     opts = [opts_key,opts_value]
-    #     # output = 'out1.png'
-    #     vocabulary = 'lvis'
-    #     webcam = None
-    #     ####### DONE #######
-
-    #     # args = get_parser().parse_args()
-    #     # setup_logger(name="fvcore")
-    #     # logger = setup_logger()
-    #     # logger.info("Arguments: " + str(args))
-
-    #     args = get_parser().parse_args(["--config-file",config_file,"--webcam",webcam,"--input",input,"--output",output,"--vocabulary",vocabulary,
-    #                                     "--opts",opts])
-    #     setup_logger(name="fvcore")
-    #     logger = setup_logger()
-    #     logger.info("Arguments: " + str(args))
-    #     # print(str(args))
-
-    #     cfg = setup_cfg(args)
-
-    #     demo = VisualizationDemo(cfg, args)
-
-    #     if args.input:
-    #         if len(args.input) == 1:
-    #             args.input = glob.glob(os.path.expanduser(args.input[0]))
-    #             assert args.input, "The input path(s) was not found"
-    #         for path in tqdm.tqdm(args.input, disable=not args.output):
-    #             img = read_image(path, format="BGR")
-    #             start_time = time.time()
-    #             predictions, visualized_output, labels = demo.run_on_image(img)
-    #             confidences = (predictions["instances"].scores).tolist()
-    #             class_idx = (predictions["instances"].pred_classes).tolist()
-    #             class_names = labels
-    #             boxes = ((predictions["instances"].pred_boxes).tensor).tolist()
-            
-    #             # print(confidences,class_names,boxes)
-    #             logger.info(
-    #                 "{}: {} in {:.2f}s".format(
-    #                     path,
-    #                     "detected {} instances".format(len(predictions["instances"]))
-    #                     if "instances" in predictions
-    #                     else "finished",
-    #                     time.time() - start_time,
-    #                 )
-    #             )
-
-    #             if args.output:
-    #                 if os.path.isdir(args.output):
-    #                     assert os.path.isdir(args.output), args.output
-    #                     out_filename = os.path.join(args.output, os.path.basename(path))
-    #                 else:
-    #                     assert len(args.input) == 1, "Please specify a directory with args.output"
-    #                     out_filename = args.output
-    #                 visualized_output.save(out_filename)
-    #             else:
-    #                 cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-    #                 cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-    #                 if cv2.waitKey(0) == 27:
-    #                     break  # esc to quit
-    #             return boxes, class_names
-    #     elif args.webcam:
-    #         assert args.input is None, "Cannot have both --input and --webcam!"
-    #         assert args.output is None, "output not yet supported with --webcam!"
-    #         if args.webcam == "screen":
-    #             cam = ScreenGrab()
-    #         else:
-    #             cam = cv2.VideoCapture(int(args.webcam))
-    #         for vis in tqdm.tqdm(demo.run_on_video(cam)):
-    #             cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-    #             cv2.imshow(WINDOW_NAME, vis)
-    #             if cv2.waitKey(1) == 27:
-    #                 break  # esc to quit
-    #         cam.release()
-    #         cv2.destroyAllWindows()
-    #     elif args.video_input:
-    #         video = cv2.VideoCapture(args.video_input)
-    #         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    #         frames_per_second = video.get(cv2.CAP_PROP_FPS)
-    #         num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    #         basename = os.path.basename(args.video_input)
-    #         codec, file_ext = (
-    #             ("x264", ".mkv") if test_opencv_video_format("x264", ".mkv") else ("mp4v", ".mp4")
-    #         )
-    #         if codec == ".mp4v":
-    #             warnings.warn("x264 codec not available, switching to mp4v")
-    #         if args.output:
-    #             if os.path.isdir(args.output):
-    #                 output_fname = os.path.join(args.output, basename)
-    #                 output_fname = os.path.splitext(output_fname)[0] + file_ext
-    #             else:
-    #                 output_fname = args.output
-    #             assert not os.path.isfile(output_fname), output_fname
-    #             output_file = cv2.VideoWriter(
-    #                 filename=output_fname,
-    #                 # some installation of opencv may not support x264 (due to its license),
-    #                 # you can try other format (e.g. MPEG)
-    #                 fourcc=cv2.VideoWriter_fourcc(*codec),
-    #                 fps=float(frames_per_second),
-    #                 frameSize=(width, height),
-    #                 isColor=True,
-    #             )
-    #         assert os.path.isfile(args.video_input)
-    #         for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-    #             if args.output:
-    #                 output_file.write(vis_frame)
-    #             else:
-    #                 cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-    #                 cv2.imshow(basename, vis_frame)
-    #                 if cv2.waitKey(1) == 27:
-    #                     break  # esc to quit
-    #         video.release()
-    #         if args.output:
-    #             output_file.release()
-    #         else:
-    #             cv2.destroyAllWindows()
-
-          
